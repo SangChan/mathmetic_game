be30b5ac0d37ebc850b28a569c1fbf6cac761cf7
SangChan Lee <sangchan82@gmail.com>
2014-10-14 16:11:02 +0900

    add event

--- src/app.js
+++ src/app.js
@@ -1,368 +1,247 @@
-var GameMainLayer = cc.Layer.extend({
-    ctor:function (){
-        var size = cc.director.getWinSize();
+var size;
+function makeImage(src, argx, argy) {
+    var imageObject = cc.Sprite.create(src);
+    imageObject.attr({
+        x: argx * 2,
+        y: size.height - (argy * 2),
+        anchorX: 0,
+        anchorY : 1.0
+    });
+    return imageObject;
+}
+function makeLabel(text, textSize, argx, argy, argColor) {
+    var labelObject = cc.LabelTTF.create(text, "Arial", textSize * 2);
+    labelObject.attr({
+        x: argx * 2,
+        y: size.height - (argy * 2), 
+        anchorX : 0,
+        anchorY : 1.0,
+        color : argColor
+    });
+    return labelObject;
+}
+
+function makeMenuItem(srcNormal, srcHighlighted, callback, object, argx, argy, tag) {
+    var menuItemObject = cc.MenuItemImage.create(
+        srcNormal,
+        srcHighlighted,
+        null,
+        callback,
+        object);
+    menuItemObject.attr({
+        x: argx * 2,
+        y: argy * 2,
+        anchorX: 0,
+        anchorY: 1.0,
+        tag : tag
+    });
+    return menuItemObject;
+}
+
+var CarSprite = cc.Sprite.extend({
+    expressionLabel : null,
+    ctor:function(src, x, y){
         this._super();
-
-        var image_car = cc.Sprite.create(res.image_car);
-        image_car.attr({
-            x: 282 * 2,
-            y: size.height - (202 * 2),
+        this.initWithFile(src);
+        this.attr({
+            x: x,
+            y: y,
             anchorX: 0,
             anchorY : 1.0
         });
-        this.addChild(image_car, 1);
-
-        var image_levelguage_top = cc.Sprite.create(res.image_levelguage_top);
-        image_levelguage_top.attr({
-            x: 1147 * 2,
-            y: size.height - (432 * 2),
+        that = this;
+        expressionLabel = cc.LabelTTF.create("1 + 1", "Arial", 50 * 2);
+        expressionLabel.attr({
+            x: 71 * 2,
+            y: - 13 * 2,
             anchorX: 0,
-            anchorY : 1.0
+            anchorY : - 1.0,
+            color : cc.color(255,255,255)
         });
+        this.addChild(expressionLabel,1);
+        this.setExpression("+",Math.floor(Math.random() * 10) + 1,Math.floor(Math.random() * 10) + 1);
+    },
+    setExpression:function(arithmetic, augend, addend) {
+        expressionLabel.setString(augend + " " + arithmetic + " " + addend);
+    }
+});
+
+var GameMainLayer = cc.Layer.extend({
+    carSprite : null,
+    answerLabel : null,
+    levelLabel : null,
+    levelDescLabel : null,
+    goldCoinLabel : null,
+    silverCoinLabel : null,
+    copperConinLabel : null,
+    ctor:function (){
+        this._super();
+        var image_levelguage_top = makeImage(res.image_levelguage_top, 1147, 432);
         this.addChild(image_levelguage_top, 1);
 
         for (var i = 1; i <= 8; i++) {
-            var image_levelguage_center = cc.Sprite.create(res.image_levelguage_center);
-            image_levelguage_center.attr({
-                x: 1147 * 2,
-                y: size.height - (432 * 2) - 64 * i,
-                anchorX: 0,
-                anchorY : 1.0
-            });
+            var image_levelguage_center = makeImage(res.image_levelguage_center, 1147, 432 + (32 * i));
             this.addChild(image_levelguage_center, 1);
         };
 
-        var image_levelguage_below = cc.Sprite.create(res.image_levelguage_below);
-        image_levelguage_below.attr({
-            x: 1147 * 2,
-            y: size.height - (432 * 2) - 64*9,
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_levelguage_below = makeImage(res.image_levelguage_below, 1147, 432 + (32 * 9));
         this.addChild(image_levelguage_below, 1);
 
-        var image_combo_1 = cc.Sprite.create(res.image_combo_1);
-        image_combo_1.attr({
-            x: 30 * 2,
-            y: size.height - (429 * 2) - (52 * 9),
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_combo_1 = makeImage(res.image_combo_1, 30, 429 + (26 * 9));
         this.addChild(image_combo_1, 1);
 
-        var image_combo_5 = cc.Sprite.create(res.image_combo_5);
-        image_combo_5.attr({
-            x: 30 * 2,
-            y: size.height - (429 * 2) - (52 * 10),
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_combo_5 = makeImage(res.image_combo_5, 30, 429 + (26 * 10));
         this.addChild(image_combo_5, 1);
 
-        var image_combo_10 = cc.Sprite.create(res.image_combo_10);
-        image_combo_10.attr({
-            x: 30 * 2,
-            y: size.height - (429 * 2) - (52 * 11),
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_combo_10 = makeImage(res.image_combo_10, 30, 429 + (26 * 11));
         this.addChild(image_combo_10, 1);
 
+        levelLabel = makeLabel("LEVEL 10", 21, 170, 22, cc.color(255,255,255));
+        this.addChild(levelLabel, 1);
+
+        levelDescLabel = makeLabel("합피감수 10인 덧,뺄셈", 40, 157, 60, cc.color(74,39,8));
+        this.addChild(levelDescLabel, 1);
+
+        goldCoinLabel = makeLabel("00", 39, (515 + 119), 43, cc.color(103,66,70));
+        this.addChild(goldCoinLabel, 1);
+
+        silverCoinLabel = makeLabel("00", 39, (515 + 276), 43, cc.color(103,66,70));
+        this.addChild(silverCoinLabel, 1);
+
+        copperCoinLabel = makeLabel("00", 39, (515 + 432), 43, cc.color(103,66,70));
+        this.addChild(copperCoinLabel, 1);
+
+        answerLabel = makeLabel("0", 40, 217 + 20 , 485 , cc.color(255,255,255));
+        this.addChild(answerLabel, 1);
+
+        this.createCar();
+
         return true;
+    },
+    createCar : function() {
+        carSprite = new CarSprite(res.image_car,-700,size.height - (202 * 2));
+        this.addChild(carSprite, 1);
+
+        var actionBy = cc.MoveBy.create(10, cc.p(size.width+700, 0));
+
+        carSprite.runAction(cc.sequence(actionBy, cc.callFunc(this.onCarMoveEnded, this)));
+    },
+    removeCar : function() {
+        this.removeChild(carSprite);
+        carSprite = null;
+    },
+    setAnswerLabel : function(newLabel) {
+        answerLabel.setString(newLabel);
+    },
+    getAnswerLabel : function() {
+        return answerLabel.getString();
+    },
+    onCarMoveEnded : function(sender) {
+        console.log("car move ended!");
+        this.removeCar();
+        this.createCar();
     }
 });
 var InputLayer = cc.Layer.extend({
     ctor:function (){
-        var size = cc.director.getWinSize();
         this._super();
 
-        var homeItem = cc.MenuItemImage.create(
-            res.btn_home_normal,
-            res.btn_home_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        homeItem.attr({
-            x: 20 * 2,
-            y: size.height - (17 * 2),
-            anchorX: 0,
-            anchorY: 1.0
-        });
+        var homeItem = makeMenuItem(res.btn_home_normal,res.btn_home_highlighted,this.onHomeClicked,this,20,-17,101);
+        var userMNItem = makeMenuItem(res.btn_usermn_normal,res.btn_usermn_highlighted,this.onUserNMClicked,this,1074,-37,102);
 
-        var userMNItem = cc.MenuItemImage.create(
-            res.btn_usermn_normal,
-            res.btn_usermn_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        userMNItem.attr({
-            x: 1074 * 2,
-            y: size.height - (37 * 2),
-            anchorX: 0,
-            anchorY: 1.0
-        });
+        var userNameLabel = makeLabel("김재능", 25, (1074 + 56) , 53 , cc.color(49,49,49));
+        this.addChild(userNameLabel, 2);
 
         var upperMenu = cc.Menu.create(homeItem,userMNItem);
         upperMenu.x = 0;
-        upperMenu.y = 0;
+        upperMenu.y = size.height;
         this.addChild(upperMenu, 2);
 
 
-        var menuItem1 = cc.MenuItemImage.create(
-            res.btn_1_normal,
-            res.btn_1_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItem1.attr({
-            x: 143 * 0 * 2,
-            y: 0,
-            anchorX: 0,
-            anchorY: 1.0
-        });
+        var btnNormalImage = [res.btn_1_normal,res.btn_2_normal,res.btn_3_normal,res.btn_4_normal,res.btn_5_normal,res.btn_6_normal,res.btn_7_normal,res.btn_8_normal,res.btn_9_normal,res.btn_0_normal];
+        var btnSelectedImage = [res.btn_1_highlighted,res.btn_2_highlighted,res.btn_3_highlighted,res.btn_4_highlighted,res.btn_5_highlighted,res.btn_6_highlighted,res.btn_7_highlighted,res.btn_8_highlighted,res.btn_9_highlighted,res.btn_0_highlighted];
+        var menuItems = new Array(12);
+        for (var i = 0; i <= 9; i++) {
+            menuItems[i] = makeMenuItem(btnNormalImage[i],btnSelectedImage[i],this.onNumKeyClicked, this,143 * (i%5), (i >= 5) ? -110 : 0, i+1);
+        };
+        menuItems[10] = makeMenuItem(res.btn_back_normal,res.btn_back_highlighted,this.onBackKeyClicked,this,143 * 5,0,1001);
+        menuItems[11] = makeMenuItem(res.btn_enter_normal,res.btn_enter_highlighted,this.onEnterKeyClicked,this,143 * 5,-110,1002);
 
-        var menuItem2 = cc.MenuItemImage.create(
-            res.btn_2_normal,
-            res.btn_2_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItem2.attr({
-            x: 143 * 1 * 2,
-            y: 0,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var menuItem3 = cc.MenuItemImage.create(
-            res.btn_3_normal,
-            res.btn_3_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItem3.attr({
-            x: 143 * 2 * 2,
-            y: 0,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var menuItem4 = cc.MenuItemImage.create(
-            res.btn_4_normal,
-            res.btn_4_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItem4.attr({
-            x: 143 * 3 * 2,
-            y: 0,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var menuItem5 = cc.MenuItemImage.create(
-            res.btn_5_normal,
-            res.btn_5_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItem5.attr({
-            x: 143 * 4 * 2,
-            y: 0,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var menuItemBack = cc.MenuItemImage.create(
-            res.btn_back_normal,
-            res.btn_back_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItemBack.attr({
-            x: 143 * 5 * 2,
-            y: 0,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var menuItem6 = cc.MenuItemImage.create(
-            res.btn_6_normal,
-            res.btn_6_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItem6.attr({
-            x: 143 * 0 * 2,
-            y: -110 * 2,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var menuItem7 = cc.MenuItemImage.create(
-            res.btn_7_normal,
-            res.btn_7_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItem7.attr({
-            x: 143 * 1 * 2,
-            y: -110 * 2,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var menuItem8 = cc.MenuItemImage.create(
-            res.btn_8_normal,
-            res.btn_8_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItem8.attr({
-            x: 143 * 2 * 2,
-            y: -110 * 2,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var menuItem9 = cc.MenuItemImage.create(
-            res.btn_9_normal,
-            res.btn_9_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItem9.attr({
-            x: 143 * 3 * 2,
-            y: -110 * 2,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var menuItem0 = cc.MenuItemImage.create(
-            res.btn_0_normal,
-            res.btn_0_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItem0.attr({
-            x: 143 * 4 * 2,
-            y: -110 * 2,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var menuItemEnter = cc.MenuItemImage.create(
-            res.btn_enter_normal,
-            res.btn_enter_highlighted,
-            null,
-            this.onMenuCallback,
-            this);
-        menuItemEnter.attr({
-            x: 143 * 5 * 2,
-            y: -110 * 2,
-            anchorX: 0,
-            anchorY: 1.0
-        });
-
-        var numMenu = cc.Menu.create(menuItem1,menuItem2,menuItem3,menuItem4,menuItem5,menuItem6,menuItem7,menuItem8,menuItem9,menuItem0,menuItemBack,menuItemEnter);
+        var numMenu = cc.Menu.create(menuItems);
         numMenu.x = 217 *2;
         numMenu.y = size.height - (560 * 2);
         this.addChild(numMenu, 2);
 
         return true;
     },
-    onMenuCallback:function (sender) {
-       console.log("clicked");
+    onNumKeyClicked:function (sender) {
+        var numKey = sender.tag % 10;
+        var prevAnswer = gameMainLayer.getAnswerLabel();
+        if (prevAnswer === '0') {
+            gameMainLayer.setAnswerLabel(numKey);
+        }
+        else {
+            gameMainLayer.setAnswerLabel(prevAnswer+numKey);
+        }
+       
+    },
+    onBackKeyClicked:function (sender) {
+        gameMainLayer.setAnswerLabel("0");
+    },
+    onEnterKeyClicked:function (sender) {
+        console.log(gameMainLayer.getAnswerLabel());
+    },
+    onUserNMClicked:function (sender) {
+    },
+    onHomeClicked:function (sender) {
     }
 });
 var BGLayer = cc.Layer.extend({
     ctor:function (){
-        var size = cc.director.getWinSize();
         this._super();
 
-        var image_bg = cc.Sprite.create(res.image_bg);
-        image_bg.attr({
-            x: 0,
-            y: size.height,
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_bg = makeImage(res.image_bg, 0, 0);
         this.addChild(image_bg, 0);
 
-        var image_conTop_bg = cc.Sprite.create(res.image_conTop_bg);
-        image_conTop_bg.attr({
-            x: 0,
-            y: size.height,
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_conTop_bg = makeImage(res.image_conTop_bg, 0, 0);
         this.addChild(image_conTop_bg, 0);
 
-        var image_combo_bg = cc.Sprite.create(res.image_combo_bg);
-        image_combo_bg.attr({
-            x: 20 * 2,
-            y: size.height - (421 * 2),
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_combo_bg = makeImage(res.image_combo_bg, 20, 421);
         this.addChild(image_combo_bg, 0);
 
-        var image_answerboard = cc.Sprite.create(res.image_answerboard);
-        image_answerboard.attr({
-            x: 187 * 2,
-            y: size.height - (421 * 2),
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_answerboard = makeImage(res.image_answerboard, 187, 421);
         this.addChild(image_answerboard, 0);
 
-        var image_answerboard_textbar = cc.Sprite.create(res.image_answerboard_textbar);
-        image_answerboard_textbar.attr({
-            x: 217 * 2,
-            y: size.height - (481 * 2),
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var userNameLabel = makeLabel("지나가는 문제의 답을 입력하세요", 25, (187 + 35) , 440 , cc.color(113,78,47));
+        this.addChild(userNameLabel, 1);
+
+        var image_answerboard_textbar = makeImage(res.image_answerboard_textbar, 217, 481);
         this.addChild(image_answerboard_textbar, 0);
 
-        var image_levelguage_bg = cc.Sprite.create(res.image_levelguage_bg);
-        image_levelguage_bg.attr({
-            x: 1136 * 2,
-            y: size.height - (421 * 2),
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_levelguage_bg = makeImage(res.image_levelguage_bg, 1136, 421);
         this.addChild(image_levelguage_bg, 0);
 
-        var image_conTop_level = cc.Sprite.create(res.image_conTop_level);
-        image_conTop_level.attr({
-            x: 154 * 2,
-            y: size.height - (19 * 2),
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_conTop_level = makeImage(res.image_conTop_level, 154, 19);
         this.addChild(image_conTop_level, 0);
 
-        var image_conTop_coinbar = cc.Sprite.create(res.image_conTop_coinbar);
-        image_conTop_coinbar.attr({
-            x: 515 * 2,
-            y: size.height - (20 * 2),
-            anchorX: 0,
-            anchorY : 1.0
-        });
+        var image_conTop_coinbar = makeImage(res.image_conTop_coinbar, 515, 20);
         this.addChild(image_conTop_coinbar, 0);
 
         return true;
     }
 });
 
+var gameMainLayer;
+
 var GameMainScene = cc.Scene.extend({
     onEnter: function () {
         this._super();
-        var bgLayer = new BGLayer();
+        size = cc.director.getWinSize();
+
+        var bgLayer= new BGLayer();
         this.addChild(bgLayer);
 
-        var gameMainLayer = new GameMainLayer();
+        gameMainLayer = new GameMainLayer();
         this.addChild(gameMainLayer);
         
         var inputLayer = new InputLayer();
@@ -370,74 +249,3 @@ var GameMainScene = cc.Scene.extend({
         
     }
 });
-
-var HelloWorldLayer = cc.Layer.extend({
-    sprite:null,
-    ctor:function () {
-        //////////////////////////////
-        // 1. super init first
-        this._super();
-
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask director the window size
-        var size = cc.director.getWinSize();
-
-        // add a "close" icon to exit the progress. it's an autorelease object
-        var closeItem = cc.MenuItemImage.create(
-            res.CloseNormal_png,
-            res.CloseSelected_png,
-            function () {
-                cc.log("Menu is clicked!");
-            }, this);
-        closeItem.attr({
-            x: size.width - 20,
-            y: 20,
-            anchorX: 0.5,
-            anchorY: 0.5
-        });
-
-        var menu = cc.Menu.create(closeItem);
-        menu.x = 0;
-        menu.y = 0;
-        this.addChild(menu, 1);
-
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = cc.LabelTTF.create("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = 0;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
-
-        // add "HelloWorld" splash screen"
-        this.sprite = cc.Sprite.create(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2,
-            scale: 0.5,
-            rotation: 180
-        });
-        this.addChild(this.sprite, 0);
-
-        var rotateToA = cc.RotateTo.create(2, 0);
-        var scaleToA = cc.ScaleTo.create(2, 1, 1);
-
-        this.sprite.runAction(cc.Sequence.create(rotateToA, scaleToA));
-        helloLabel.runAction(cc.Spawn.create(cc.MoveBy.create(2.5, cc.p(0, size.height - 40)),cc.TintTo.create(2.5,255,125,0)));
-        return true;
-    }
-});
-
-var HelloWorldScene = cc.Scene.extend({
-    onEnter:function () {
-        this._super();
-        var layer = new HelloWorldLayer();
-        this.addChild(layer);
-    }
-});
-
